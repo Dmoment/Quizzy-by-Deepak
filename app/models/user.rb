58e448ddf47576a99ca576bd38fff14c2281fe9e
Deepak Chauhan <rishudc119@gmail.com>
2021-02-06 21:59:28 +0530

    Added unit tests for user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,14 @@
 class User < ApplicationRecord
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :email, presence: true
-  validates :first_name, length: { maximum: 50 }
-  validates :last_name, length: { maximum: 50 }
-  validates :email, uniqueness: true
+  
+  validates :first_name, presence: true, length: { maximum: 50 }
+  validates :last_name, presence: true,  length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$\z/  
+  validates :email, presence: true, uniqueness: true, 
+                    format: {with: VALID_EMAIL_REGEX}
+  before_save :normalize_email
+  
+  def normalize_email
+    self.email.downcase!
+  end
+  
 end
