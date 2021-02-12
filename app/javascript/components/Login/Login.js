bdb8f6b8b6261069295b67310e15ead99c201bb7
Deepak Chauhan <rishudc119@gmail.com>
2021-02-12 11:47:22 +0530

    checking duplication

--- app/javascript/components/Login/Login.js
+++ app/javascript/components/Login/Login.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import axios from 'axios';
-import {handleErrorResponse, handleSuccessResponse} from "../../packs/apis/axios"
+import {handleErrorResponse, handleSuccessResponse} from "../../src/apis/axios"
 
 
 function Login() {
